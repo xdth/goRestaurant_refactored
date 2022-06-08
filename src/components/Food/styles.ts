114acc0d653bb7689765931b88ee61993e74b478
xdth <info@dthlabs.com>
2022-06-08 18:27:43 -0400

    Refactoring completed

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface IFoodPlateProps {
+  available: boolean;
+}
+
+export const Container = styled.div<IFoodPlateProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
@@ -13,8 +17,8 @@ export const Container = styled.div`
     text-align: center;
 
     ${props =>
-    !props.available &&
-    css`
+      !props.available &&
+      css`
         opacity: 0.3;
       `};
 
